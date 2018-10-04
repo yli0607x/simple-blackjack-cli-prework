34c42458b2e4b6cc117d5dc7d2f562b67814cf87
Yu Li <yli0607x@gmail.com>
2018-10-04 23:20:18 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -47,5 +47,11 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  total = initial_round
+  until total > 21 
+    total = hit?(total)
+    display_card_total(total)
+end
+end_game(total)
 end
