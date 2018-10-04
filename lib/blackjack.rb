6bc49ea4fcbc644be4ef7a96df8f40ea16e31794
Yu Li <yli0607x@gmail.com>
2018-10-04 23:13:28 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -18,8 +18,8 @@ def get_user_input
   gets.chomp
 end
 
-def end_game
-  print "Sorry, you hit #{card_total}. Thanks for playing!"
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
